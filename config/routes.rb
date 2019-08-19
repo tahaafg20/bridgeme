19c72897bcb21a1f4eb50ae89904854db5cb41ea
Taha <baktash.29.ra@gmail.com>
2019-08-19 17:39:04 +0800

    added sidekiq dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
- 
+  require 'sidekiq/web'
+  mount Sidekiq::Web => '/sidekiq'
   resources :organizations
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks',  registrations: 'users/registrations'}
   devise_scope :users do
