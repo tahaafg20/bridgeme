75da95acd58ead4e853863eb8b0f42659285aee8
taghreed <taghreed.alqubati@gmail.com>
2019-08-20 09:20:50 +0800

    new brunch

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+  require 'sidekiq/web'
+  mount Sidekiq::Web => '/sidekiq'
  
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks',  registrations: 'users/registrations'}
   devise_scope :users do
