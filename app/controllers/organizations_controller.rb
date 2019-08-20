43e889827564fe31df48379bb799a12da0f05936
Taha <baktash.29.ra@gmail.com>
2019-08-20 10:06:50 +0800

    working on show pages

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -69,6 +69,6 @@ class OrganizationsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def organization_params
-      params.require(:organization).permit(:name, :email, :number, :address, :service, :about)
+      params.require(:organization).permit(:name, :email, :number, :address, :service, :about, :latitude, :longitude)
     end
 end
