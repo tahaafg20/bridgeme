c3a01cf2270929d84872c25a03ad6a0d1860b348
taghreed <taghreed.alqubati@gmail.com>
2019-08-21 12:22:27 +0800

    from organization brunch

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_19_100854) do
+ActiveRecord::Schema.define(version: 2019_08_20_081913) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
