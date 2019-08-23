b2fc1b79f7005c6b4d744dd9b0a0e0cafac7586b
Taha <baktash.29.ra@gmail.com>
2019-08-23 13:48:00 +0800

    fixed messaging

--- app/models/message.rb
+++ app/models/message.rb
@@ -15,10 +15,10 @@ class Message < ApplicationRecord
     def create_notification
       if self.conversation.sender_id == self.user_id
         sender = User.find(self.conversation.sender_id)
-        Notification.create(content: "New message from #{sender.fullname}", user_id: self.conversation.recipient_id)
+        Notification.create(content: "New message from #{sender.fullname}", user_id: self.conversation.recipient_id,message_id: self.id)
       else
         sender = User.find(self.conversation.recipient_id)
-        Notification.create(content: "New message from #{sender.fullname}", user_id: self.conversation.sender_id)
+        Notification.create(content: "New message from #{sender.fullname}", user_id: self.conversation.sender_id, message_id: self.id)
       end
     end
 end
