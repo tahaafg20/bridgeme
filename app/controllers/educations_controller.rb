01fdf2cecefab08f46a1e776c8e4ab32933e070c
Taha <baktash.29.ra@gmail.com>
2019-08-23 17:28:52 +0800

    finished health

--- app/controllers/educations_controller.rb
+++ app/controllers/educations_controller.rb
@@ -31,6 +31,7 @@ class EducationsController < ApplicationController
 
     respond_to do |format|
       if @education.save
+        byebug
         format.html { redirect_to @education, notice: 'Education was successfully created.' }
         format.json { render :show, status: :created, location: @education }
       else
