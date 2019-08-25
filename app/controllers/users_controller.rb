1ad3ceae80226d8d2293643b0d3c3bc826c24555
taghreed <taghreed.alqubati@gmail.com>
2019-08-25 14:01:14 +0800

    fix comment reviw

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,10 +6,10 @@ class UsersController < ApplicationController
     @rooms = @user.rooms
 
     # Display all the guest reviews to host (if this user is a host)
-    @guest_reviews = Review.where(type: "GuestReview", host_id: @user.id)
+    @guest_reviews = Review.where(type: "GuestReview", host_id:current_user,guest_id: @user.id)
 
     # Display all the host reviews to host (if this user is a guest)
-    @host_reviews = Review.where(type: "HostReview", guest_id: @user.id)
+    @host_reviews = Review.where(type: "HostReview", guest_id:current_user ,host_id:@user.id)
   end
 
 
