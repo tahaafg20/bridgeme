576d1ddfaa2fbd319dd0171cbdf7b06a83e82f9e
Taha <baktash.29.ra@gmail.com>
2019-08-26 23:58:57 +0800

    added facebook

--- db/migrate/20190826051743_add_link_to_orgs.rb
+++
@@ -1,6 +0,0 @@
-class AddLinkToOrgs < ActiveRecord::Migration[6.0]
-  def change
-    add_column :communities, :link, :string
-    add_column :educations, :link, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_26_051743) do
+ActiveRecord::Schema.define(version: 2019_08_25_042835) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,7 +60,6 @@ ActiveRecord::Schema.define(version: 2019_08_26_051743) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id"
-    t.string "link"
     t.index ["user_id"], name: "index_communities_on_user_id"
   end
 
@@ -85,7 +84,6 @@ ActiveRecord::Schema.define(version: 2019_08_26_051743) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "address"
     t.bigint "user_id"
-    t.string "link"
     t.index ["user_id"], name: "index_educations_on_user_id"
   end
 
