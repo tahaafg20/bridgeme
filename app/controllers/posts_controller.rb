188bf02e95bbf155888351c8c3c16c0bace3bea5
taghreed <taghreed.alqubati@gmail.com>
2019-08-26 17:51:20 +0800

    check post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -25,7 +25,8 @@ class PostsController < ApplicationController
   # POST /posts.json
   def create
    
-    @post = Post.new(post_params)
+  byebug
+    @post = current_user.posts.build(post_params)
     respond_to do |format|
       if @post.save
         @post.reindex
