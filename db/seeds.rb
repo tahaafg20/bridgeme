36cbdb3bcfa38e58b3b38bcb55bb771a1afad6e9
Jack <superjackpower85@gmail.com>
2019-08-27 18:33:41 +0800

    listing html css inline added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# image1 = File.read('path/to/image.jpg')
+
+
+
+# Room.create!([
+#     {
+#     room_type: "Small Room",   
+#     listing_name: "Room is a 2015 independent drama film",
+#     summary: "novel of the same name. It stars Brie Larson as a woman who has been held captive for seven years, and whose 5-year-old son ",
+#     accomodate: 3,
+#     address: "Pudu Plaza, Jalan Pudu, Pudu, Kuala Lumpur, Federal Territory of Kuala Lumpur, Malaysia",
+#     is_tv: 'Yes',
+#     is_heating: 'No',
+#     is_air: 'No',
+#     is_kitchen: 'Yes',
+#     is_internet: 'No',
+#     price: 100,
+#     active: 'No',
+#     user_id: 10,
+#     created_at: "2019-08-27 10:19:24",
+#     updated_at: "2019-08-27 10:19:24",
+#     latitude: 'nil',
+#     longitude: 'nil',
+#     instant: "Instant"
+#     }
+
+#   ])
